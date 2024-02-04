75c21ba2e6647a0c9a77aedc21129885c3517976
Ziang Tong <97473223+DannyTDS@users.noreply.github.com>
2024-02-04 18:01:18 -0500

    antialiasing

--- include/utils.h
+++ include/utils.h
@@ -5,6 +5,7 @@
 #include <limits>
 #include <memory>
 #include <algorithm>
+#include <cstdlib>
 
 // Usings
 
@@ -23,6 +24,26 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+/**
+ * @brief Returns a random real in [0,1).
+ * 
+ * @return double 
+ */
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+/**
+ * @brief Returns a random real in [min,max).
+ * 
+ * @return double 
+ */
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 // Common Headers
